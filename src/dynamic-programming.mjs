5eb283ef8918d793a40b8fd7658294f956f15c10
felip <ph.bell.cpe@gmail.com>
2020-01-21 14:01:24 -0500

    subset sum working brute force

--- src/dynamic-programming.mjs
+++ src/dynamic-programming.mjs
@@ -1,18 +1,46 @@
 /*
+Given a set of positive numbers, find if we can partition it into two subsets such that the sum of elements in both the subsets is equal.
+ */
+export function equalSubsetSum(arr)
+{
+  let sum1 = 0;
+  let sum2 = 0;
+
+  function subsetRecursive(sum1, sum2, arr, currentIndex)
+  {
+    if (currentIndex >= arr.length)
+    {
+      return sum1 === sum2;
+    }
+
+    const first = subsetRecursive(sum1 + arr[currentIndex], sum2, arr,
+        currentIndex + 1);
+    const second = subsetRecursive(sum1, sum2 + arr[currentIndex], arr,
+        currentIndex + 1);
+
+    return ( first || second );
+  }
+
+  return subsetRecursive(sum1, sum2, arr, 0);
+}
+
+/*
 0/1knapsack
 Given the weights and profits of ‘N’ items, we are asked to put these items in a knapsack which has a capacity ‘C’. The goal is to get the maximum profit from the items in the knapsack. Each item can only be selected once, as we don’t have multiple quantities of any item.
  */
-
-export function solveKnapsack(capacity, profits, weights)
+export function solveKnapsackMemo(capacity, profits, weights)
 {
-  const memo = []
-  function knapsackRecursive(profits, weights, capacity, currentIndex) {
+  const memo = [];
+
+  function knapsackRecursive(profits, weights, capacity, currentIndex)
+  {
     // base checks
     if (capacity <= 0 || currentIndex >= profits.length) return 0;
 
     // have we seen this before?
     memo[currentIndex] = memo[currentIndex] || [];
-    if (typeof memo[currentIndex][capacity] !== 'undefined') {
+    if (typeof memo[currentIndex][capacity] !== 'undefined')
+    {
       return memo[currentIndex][capacity];
     }
     // recursive call after choosing the element at the currentIndex
@@ -25,11 +53,68 @@ export function solveKnapsack(capacity, profits, weights)
     }
 
     // recursive call after excluding the element at the currentIndex
-    const profit2 = knapsackRecursive(profits, weights, capacity, currentIndex + 1);
+    const profit2 = knapsackRecursive(profits, weights, capacity,
+        currentIndex + 1);
 
     memo[currentIndex][capacity] = Math.max(profit1, profit2);
     return memo[currentIndex][capacity];
   }
 
   return knapsackRecursive(profits, weights, capacity, 0);
+}
+
+export function solveKnapsackBott(capacity, profits, weights)
+{
+  const n = profits.length;
+  if (capacity <= 0 || n == 0 || weights.length != n) return 0;
+
+  const dp = Array(profits.length).
+      fill(0).
+      map(() => Array(capacity + 1).fill(0));
+
+  // populate the capacity=0 columns; with '0' capacity we have '0' profit
+  for (let i = 0; i < n; i++) dp[i][0] = 0;
+
+  // if we have only one weight, we will take it if it is not more than the capacity
+  for (let c = 0; c <= capacity; c++)
+  {
+    if (weights[0] <= c) dp[0][c] = profits[0];
+  }
+
+  // process all sub-arrays for all the capacities
+  for (let i = 1; i < n; i++)
+  {
+    for (let c = 1; c <= capacity; c++)
+    {
+      let profit1 = 0,
+          profit2 = 0;
+      // include the item, if it is not more than the capacity
+      if (weights[i] <= c) profit1 = profits[i] + dp[i - 1][c - weights[i]];
+      // exclude the item
+      profit2 = dp[i - 1][c];
+      // take maximum
+      dp[i][c] = Math.max(profit1, profit2);
+    }
+  }
+
+  let selectedWeights = '';
+  let totalProfit = dp[weights.length - 1][capacity];
+  let remainingCapacity = capacity;
+  for (let i = weights.length - 1; i > 0; i--)
+  {
+    if (totalProfit != dp[i - 1][remainingCapacity])
+    {
+      selectedWeights = `${ weights[i] } ${ selectedWeights }`;
+      remainingCapacity -= weights[i];
+      totalProfit -= profits[i];
+    }
+  }
+
+  if (totalProfit !=
+      0) selectedWeights = `${ weights[0] } ${ selectedWeights }`;
+
+  console.log(`Selected weights: ${ selectedWeights }`);
+
+  // maximum profit will be at the bottom-right corner.
+  return dp[n - 1][capacity];
 }
--- test/dynamic-programming-spec.mjs
+++ test/dynamic-programming-spec.mjs
@@ -2,24 +2,59 @@ import test from 'tape'
 import * as dp from '../src/dynamic-programming.mjs'
 
 test("return maximum profits given weights, profits, and capacity", (assert) => {
-  let profits = [1, 6, 10, 16]
-  let weights = [1, 2, 3, 5]
-  let capacities = [7, 6]
-  let expected = [22,17]
+  let profits = [1, 6, 10, 16];
+  let weights = [1, 2, 3, 5];
+  let capacities = [7, 6];
+  let expected = [22, 17];
 
-  capacities.forEach(function( item, i){
-    const actual = dp.solveKnapsack( item, profits, weights )
+  capacities.forEach(function(item, i)
+  {
+    const actual = dp.solveKnapsackMemo(item, profits, weights);
     assert.equal(actual, expected[i],
         item + ' => ' + expected[i]);
   });
 
-  profits = [4, 5, 3, 7]
-  weights = [2, 3, 1, 4]
-  capacities = [5]
-  expected = [10]
+  capacities.forEach(function(item, i)
+  {
+    const actual = dp.solveKnapsackBott(item, profits, weights);
+    assert.equal(actual, expected[i],
+        item + ' => ' + expected[i]);
+  });
 
-  capacities.forEach(function( item, i){
-    const actual = dp.solveKnapsack( item, profits, weights )
+  profits = [4, 5, 3, 7];
+  weights = [2, 3, 1, 4];
+  capacities = [5];
+  expected = [10];
+
+  capacities.forEach(function(item, i)
+  {
+    const actual = dp.solveKnapsackMemo(item, profits, weights);
+    assert.equal(actual, expected[i],
+        item + ' => ' + expected[i]);
+  });
+
+  capacities.forEach(function(item, i)
+  {
+    const actual = dp.solveKnapsackBott(item, profits, weights);
+    assert.equal(actual, expected[i],
+        item + ' => ' + expected[i]);
+  });
+
+  assert.end();
+});
+
+test('check if two subsets can be created that have equal sums', (assert) =>
+{
+  const input = [
+    [1, 2, 3, 4]
+    , [1, 1, 3, 4, 7]
+    , [2, 3, 4, 6],
+  ];
+  const expected = [true, true, false];
+
+  input.forEach(function(item, i)
+  {
+    const actual = dp.equalSubsetSum(item);
     assert.equal(actual, expected[i],
         item + ' => ' + expected[i]);
   });
